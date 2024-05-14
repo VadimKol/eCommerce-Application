e62a2c55e5317497b124502e145942430cd25551
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-05-14 15:26:18 +0200

    feat: ECOMM-2_0 add pages for shop (#8)

--- src/main.tsx
+++ src/main.tsx
@@ -5,5 +5,4 @@ const rootElement = document.createElement('div');
 
 rootElement.className = 'root';
 document.body.insertBefore(rootElement, document.body.firstChild);
-
 ReactDOM.createRoot(rootElement).render(<React.StrictMode />);
