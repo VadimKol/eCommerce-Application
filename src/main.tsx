ce85de624a46f8d38005cb99aaf4afa69d4132b2
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-05-15 13:49:39 +0200

    feat: ECOMM-2_09 Add Validation For Registration (#10)

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,9 @@ import { About } from './pages/about/About';
 import { NoMatch } from './pages/no-match/NoMatch';
 import { ErrorPage } from './pages/error/Error';
 import { LoginProvider } from './contexts/login-context';
+import './style/index.scss';
 
+document.body.classList.add('body');
 const rootElement = document.createElement('div');
 
 rootElement.className = 'root';
