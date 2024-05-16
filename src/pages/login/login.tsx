0f71a81b62b9001d25ec08ab2274616a5c725f02
Vadim Kolymbet <3t0n@inbox.ru>
2024-05-16 16:31:12 +0300

    feat: upgrade layout and add reveal password button

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,84 +1,119 @@
 import { useForm } from 'react-hook-form';
-// import type { SubmitHandler } from 'react-hook-form';
 import './login.scss';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useState } from 'react';
 
 const formSchema = z.object({
-  email: z.string().email('Incorrect email'),
+  email: z
+    .string()
+    .email(`Email addresses must contain both a local part and a domain name separated by an '@' symbol.`),
   password: z
     .string()
     .min(8, 'Minimum 8 characters')
-    .refine((val) => /[A-Z]/.test(val), 'Password must contain at least one uppercase letter')
-    .refine((val) => /[a-z]/.test(val), 'Password must contain at least one lowercase letter')
-    .refine((val) => /[0-9]/.test(val), 'Password must contain at least one digit')
+    .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
+    .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
+    .regex(/[0-9]/, 'Password must contain at least one digit')
     .refine(
       (val) => val[0] !== ' ' && val[val.length - 1] !== ' ',
       'Password must not contain leading or trailing whitespace.',
     ),
 });
-// .required(); // толку нет от обязательности, isValid не даст зайти все равно
 
 type FormSchema = z.infer<typeof formSchema>;
 
 export function Login(): JSX.Element {
+  const [revealPassword, setRevealPassword] = useState(false);
   const {
     register,
-    // handleSubmit,
-    // reset,
+    getFieldState,
     formState: { errors, isValid },
   } = useForm<FormSchema>({ mode: 'onChange', resolver: zodResolver(formSchema) });
   const { onChange: onChangeEmail, name: Email, ref: refEmail } = register('email');
   const { onChange: onChangePassword, name: Password, ref: refPassword } = register('password');
+  const emailValue = getFieldState('email');
+  const passwordValue = getFieldState('password');
+
+  let emailClass = 'email';
+  let passwordClass = 'password';
 
   /*   const onSubmit: SubmitHandler<FormSchema> = (data) => {
     console.log(data);
     // reset();
   }; */
 
+  if (emailValue.isDirty) {
+    emailClass += emailValue.invalid ? ' invalid' : ' valid';
+  }
+
+  if (passwordValue.isDirty) {
+    passwordClass += passwordValue.invalid ? ' invalid' : ' valid';
+  }
+
   return (
     <form
       className="login"
       onSubmit={(event) => {
         // handleSubmit(onSubmit)(event).catch((err) => console.log(err));
-        event.preventDefault(); // это можно не писать, если валидировать на сабмите
+        event.preventDefault();
+        // navigate('/');
       }}
     >
-      <input
-        onChange={(event) => {
-          onChangeEmail(event).catch(() => {});
-        }}
-        name={Email}
-        ref={refEmail}
-        className="email"
-        type="email" // не будет ошибки с пробелами в начале и в конце, так как удаляются, нужен тип text
-        placeholder="name@mail.com"
-        aria-invalid={errors.email ? 'true' : 'false'}
-      />
+      <h2 className="login-title">Login</h2>
+      <label htmlFor="email-login" className="email-label">
+        Email:
+        <input
+          onChange={(event) => {
+            onChangeEmail(event).catch(() => {});
+          }}
+          name={Email}
+          ref={refEmail}
+          id="email-login"
+          className={emailClass}
+          type="email"
+          placeholder="user@example.com"
+          aria-invalid={errors.email ? 'true' : 'false'}
+        />
+      </label>
       {errors.email && (
         <span role="alert" className="error">
           {errors.email.message}
         </span>
       )}
-      <input
-        onChange={(event) => {
-          onChangePassword(event).catch(() => {});
-        }}
-        name={Password}
-        ref={refPassword}
-        className="password"
-        type="password"
-        placeholder="password"
-        aria-invalid={errors.password ? 'true' : 'false'}
-      />
+      <div className="password-block">
+        <label htmlFor="password-login" className="password-label">
+          Password:
+          <input
+            onChange={(event) => {
+              onChangePassword(event).catch(() => {});
+            }}
+            name={Password}
+            ref={refPassword}
+            id="password-login"
+            className={passwordClass}
+            type={revealPassword ? 'text' : 'password'}
+            placeholder="password"
+            aria-invalid={errors.password ? 'true' : 'false'}
+          />
+        </label>
+        <button
+          type="button"
+          className={revealPassword ? 'reveal show' : 'reveal'}
+          aria-label="Reveal"
+          onClick={() => setRevealPassword(!revealPassword)}
+        />
+      </div>
       {errors.password && (
         <span role="alert" className="error">
           {errors.password.message}
         </span>
       )}
-      <button type="submit" className="login-button" disabled={!isValid}>
-        Login
+      <button type="submit" className={!isValid ? 'login-button disabled' : 'login-button'}>
+        Log in
       </button>
+      <p className="registry-link">
+        Haven’t registered yet? Sign up {/* <Link> */}here{/* </Link> */}
+      </p>
     </form>
   );
 }
