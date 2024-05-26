d4981108364edadd8035ab635839a7fe753458d8
Izy <126877709+izy-code@users.noreply.github.com>
2024-05-26 19:43:18 +0300

    feat: ECOMM-3_19-22 improve routing and header

--- src/components/header/burger-menu/BurgerMenu.tsx
+++ src/components/header/burger-menu/BurgerMenu.tsx
@@ -1,12 +1,25 @@
 import './react-burger-menu-styles.scss';
 
+import { useEffect, useState } from 'react';
 import { slide as Menu } from 'react-burger-menu';
+import { useLocation } from 'react-router-dom';
 
 import { HeaderLinks } from '../links/HeaderLinks';
 
 export function BurgerMenu(): JSX.Element {
+  const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [location]);
+
+  const handleStateChange = (state: { isOpen: boolean }): void => {
+    setIsOpen(state.isOpen);
+  };
+
   return (
-    <Menu right disableCloseOnEsc width={280} itemListElement="div">
+    <Menu right disableCloseOnEsc width={280} itemListElement="div" isOpen={isOpen} onStateChange={handleStateChange}>
       <HeaderLinks isInsideBurgerMenu />
     </Menu>
   );
