076a11e02255c37d0feae40e8ad44cbe9cb14b91
Vadim Kolymbet <3t0n@inbox.ru>
2024-05-29 12:50:49 +0300

    feat: update range slider for prices

--- src/components/filters/Filters.tsx
+++ src/components/filters/Filters.tsx
@@ -1,23 +1,14 @@
-import './range_slider.scss';
-
-import MultiRangeSlider, { type ChangeResult } from 'multi-range-slider-react';
 import { useState } from 'react';
 
 import { fandoms } from '@/common/utils';
 
 import { CustomButton } from '../custom-button/СustomButton';
+import { RangeSlider } from '../range-slider/RangeSlider';
 import styles from './styles.module.scss';
 
 export function Filters(): JSX.Element {
   const [isPrice, setIsPrice] = useState(false);
   const [isFranchise, setIsFranchise] = useState(false);
-
-  const [minPrice, setminPrice] = useState(250);
-  const [maxPrice, setmaxPrice] = useState(750);
-  const handleInput = (e: ChangeResult): void => {
-    setminPrice(e.minValue);
-    setmaxPrice(e.maxValue);
-  };
 
   return (
     <>
@@ -36,27 +27,7 @@ export function Filters(): JSX.Element {
             <span className={isPrice ? `${styles.icon} ${styles.icon_show}` : styles.icon} />
           </div>
           <div className={isPrice ? `${styles.range} ${styles.range_active}` : styles.range}>
-            <MultiRangeSlider
-              baseClassName="multi-range-slider-blue"
-              min={1}
-              max={1000}
-              step={1}
-              minValue={minPrice}
-              maxValue={maxPrice}
-              onInput={handleInput}
-              barLeftColor="#d3d3d3"
-              barRightColor="#d3d3d3"
-              barInnerColor="var(--primary-color)"
-              thumbLeftColor="var(--background-color)"
-              thumbRightColor="var(--background-color)"
-              ruler={false}
-              label={false}
-              style={{ border: 'none', boxShadow: 'none' }}
-            />
-            <div className={styles.prices}>
-              <span>£{minPrice}</span>
-              <span>£{maxPrice}</span>
-            </div>
+            <RangeSlider min={1} max={150} />
           </div>
         </li>
         <li>
