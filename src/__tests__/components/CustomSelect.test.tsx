eb4248433e72983fb0250cd48f7be0f75f9580ae
Vadim Kolymbet <3t0n@inbox.ru>
2024-06-08 01:38:53 +0300

    feat: update few tests and add test to toast

--- src/__tests__/components/CustomSelect.test.tsx
+++ src/__tests__/components/CustomSelect.test.tsx
@@ -1,21 +1,14 @@
 import { render } from '@testing-library/react';
-import React from 'react';
 
 import { sortingTypes } from '@/common/utils';
 import { CustomSelect } from '@/components/custom-select/CustomSelect';
 
 it('renders correctly', () => {
-  let someValue = 'Sort';
+  const someValue = 'Sort';
   const someDispatch = (): void => {};
-  const useReducer = jest.fn().mockImplementation(() => [someValue, someDispatch]);
-  jest.spyOn(React, 'useReducer').mockImplementation(useReducer);
 
-  const { container, rerender } = render(
+  const { container } = render(
     <CustomSelect selectItems={sortingTypes} selectState={someValue} dispatch={someDispatch} />,
   );
-
-  someValue = 'Price, low to high';
-
-  rerender(<CustomSelect selectItems={sortingTypes} selectState={someValue} dispatch={someDispatch} />);
   expect(container).toMatchSnapshot();
 });
--- src/__tests__/components/FormProfileAddresses.test.tsx
+++ src/__tests__/components/FormProfileAddresses.test.tsx
@@ -1,20 +1,51 @@
 import { render } from '@testing-library/react';
 
 import { FormProfileAddresses } from '@/components/form-profile-addresses/FormProfileAddresses';
+import { registerSchema } from '@/components/form-profile-addresses/register-schema';
 
-it('renders correctly', () => {
-  const setter = (): void => {};
+describe('Testing profile address component', () => {
+  test('good address not to throw', () => {
+    expect(() =>
+      registerSchema.parse({
+        street: 'a',
+        city: 'a',
+        default: true,
+        apartment: 'a',
+        country: 'RU',
+        postcode: '123456',
+        id: '12345',
+      }),
+    ).not.toThrow();
+  });
 
-  const { container } = render(
-    <FormProfileAddresses
-      version={1}
-      addresses={[]}
-      defaultAddress=""
-      isBilling={false}
-      setPersonInfo={setter}
-      setAddressesShip={setter}
-      setAddressesBill={setter}
-    />,
-  );
-  expect(container).toMatchSnapshot();
+  test('address with bad postcode will throw', () => {
+    expect(() =>
+      registerSchema.parse({
+        street: 'a',
+        city: 'a',
+        default: true,
+        apartment: 'a',
+        country: 'RU',
+        postcode: '123',
+        id: '12345',
+      }),
+    ).toThrow();
+  });
+
+  it('renders correctly', () => {
+    const setter = (): void => {};
+
+    const { container } = render(
+      <FormProfileAddresses
+        version={1}
+        addresses={[]}
+        defaultAddress=""
+        isBilling={false}
+        setPersonInfo={setter}
+        setAddressesShip={setter}
+        setAddressesBill={setter}
+      />,
+    );
+    expect(container).toMatchSnapshot();
+  });
 });
