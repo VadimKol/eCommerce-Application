f441d36acb7695f3cf7feee5b2006391b430eff9
Vadim Kolymbet <3t0n@inbox.ru>
2024-06-09 00:47:24 +0300

    feat: move pagination to center in some cases

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -4,32 +4,41 @@ import styles from './styles.module.scss';
 import { type PaginationProps } from './types';
 
 export function Pagination({ page, dispatch, total }: PaginationProps): JSX.Element {
+  const PAGE_MAX = Math.floor(total.current / QUERY_LIMIT) + (total.current % QUERY_LIMIT > 0 ? 1 : 0);
+  let paginationStyles = styles.pagination_box;
+  if (!page) {
+    paginationStyles = `${styles.pagination_box_before}`;
+  }
+  if (!(page + 1 < PAGE_MAX)) {
+    paginationStyles = `${styles.pagination_box_after}`;
+  }
   return (
     <div className={styles.pagination}>
-      {Boolean(page) && (
-        <button
-          type="button"
-          aria-label="Left"
-          className={styles.pag_left}
-          onClick={() => {
-            dispatch({ type: 'SET_PAGE', page: page - 1 });
-          }}
-        />
-      )}
-      <div className={styles.pages}>
-        <span>{page + 1}</span>-
-        <span>{Math.floor(total.current / QUERY_LIMIT) + (total.current % QUERY_LIMIT > 0 ? 1 : 0)}</span>
+      <div className={paginationStyles}>
+        {Boolean(page) && (
+          <button
+            type="button"
+            aria-label="Left"
+            className={styles.pag_left}
+            onClick={() => {
+              dispatch({ type: 'SET_PAGE', page: page - 1 });
+            }}
+          />
+        )}
+        <div className={styles.pages}>
+          <span>{page + 1}</span>-<span>{PAGE_MAX}</span>
+        </div>
+        {page + 1 < PAGE_MAX && (
+          <button
+            type="button"
+            aria-label="Right"
+            className={styles.pag_right}
+            onClick={() => {
+              dispatch({ type: 'SET_PAGE', page: page + 1 });
+            }}
+          />
+        )}
       </div>
-      {page + 1 < Math.floor(total.current / QUERY_LIMIT) + (total.current % QUERY_LIMIT > 0 ? 1 : 0) && (
-        <button
-          type="button"
-          aria-label="Right"
-          className={styles.pag_right}
-          onClick={() => {
-            dispatch({ type: 'SET_PAGE', page: page + 1 });
-          }}
-        />
-      )}
     </div>
   );
 }
