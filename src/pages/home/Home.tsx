c5d8362cd6df05548248de78df421fe80b71614d
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-06-10 15:29:58 +0200

    feat: ECOMM-4_26 add banner photos

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,45 @@
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import classNames from 'classnames';
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 import styles from './styles.module.scss';
 
 export function Home(): JSX.Element {
+  const images = [
+    '../../assets/images/promo_banner.png',
+    '../../assets/images/avatar_banner.png',
+    '../../assets/images/war_banner.png',
+  ];
   return (
     <main className={classNames('main', styles.main)}>
       <div className={styles.container}>
         <h1 className={styles.title}>Home page</h1>
-        <p className={styles.linksDesc}>Under construction...</p>
+        <Swiper
+          initialSlide={0}
+          spaceBetween={10}
+          slidesPerView={1}
+          navigation
+          lazyPreloadPrevNext={1}
+          loop
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Navigation, Pagination]}
+          className="modalSwiper"
+        >
+          {images &&
+            images.map((image, index) => (
+              <SwiperSlide key={image} className={styles.swiperSlide}>
+                <img src={image} alt={`banner number ${index + 1}`} className={styles.modalImage} loading="lazy" />
+                <div className="swiper-lazy-preloader" />
+              </SwiperSlide>
+            ))}
+        </Swiper>
+        <div>Apply Promo Code &quot;GEEK-SHOP&quot; and get 10% discount</div>
       </div>
     </main>
   );
