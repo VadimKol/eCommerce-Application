8418ef3945d9e8cd514b611ade41369d16cd7b14
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-06-14 23:00:07 +0200

    Feat/ecomm 4 26 add home page (#40)
    * feat: ECOMM-4_26 add banner photos
    
    * feat: ECOMM-4_26 add promocode line
    
    * feat: ECOMM-4_26 add separate component for promocode
    
    * feat: ECOMM-4_26 add component catalog card
    
    * fix: ECOMM-4_26 change style for layout of home page
    
    * fix: ECOMM-4_26 change profile styles
    
    * fix: ECOMM-4_26 change styles for banner at home
    
    * fix: ECOMM-4_26 change styles for promocode line
    
    * fix: ECOMM-4_26 correct layout of block to catalog
    
    * fix: ECOMM-4_26 correct style for catalogGrid component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,61 @@
-import classNames from 'classnames';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import './swiper.scss';
 
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import { CatalogGrid } from '../../components/catalog-grid/CatalogGrid.tsx';
+import { PromocodeLine } from '../../components/promocode-line/PromocodeLine.tsx';
 import styles from './styles.module.scss';
 
 export function Home(): JSX.Element {
+  const bannerImages = [
+    {
+      id: 1,
+      extraClass: 'avatar',
+      link: '/catalog/decor/figures/aang-tubbz',
+    },
+    {
+      id: 2,
+      extraClass: 'war',
+      link: '/catalog/decor/figures/stormtrooper-mini-tubbz',
+    },
+  ];
+
   return (
     <main className={classNames('main', styles.main)}>
       <div className={styles.container}>
         <h1 className={styles.title}>Home page</h1>
-        <p className={styles.linksDesc}>Under construction...</p>
+        <Swiper
+          initialSlide={0}
+          spaceBetween={15}
+          slidesPerView={1}
+          navigation
+          lazyPreloadPrevNext={1}
+          loop
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Navigation, Pagination]}
+          className="modalSwiper"
+        >
+          {bannerImages &&
+            bannerImages.map((bannerItem) => (
+              <SwiperSlide key={bannerItem.id} className={classNames(styles.swiperSlide, bannerItem.extraClass)}>
+                <Link to={bannerItem.link} className={styles.link}>
+                  <div className="hide">Banner</div>
+                </Link>
+              </SwiperSlide>
+            ))}
+        </Swiper>
+        <h2 className={styles.h2Title}>Our Products </h2>
+        <CatalogGrid />
       </div>
+      <PromocodeLine />
     </main>
   );
 }
