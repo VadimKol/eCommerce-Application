6560a5b6ba7623681de52955ad6102ed2a5f5b85
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-06-16 15:45:46 +0200

    fix: ECOMM-4_19 improve animation

--- src/components/command-block/CommandBlock.tsx
+++ src/components/command-block/CommandBlock.tsx
@@ -4,8 +4,6 @@ import { Link } from 'react-router-dom';
 import styles from './styles.module.scss';
 
 interface TextInfo {
-  title?: string;
-  github?: string;
   role?: string;
   contributions?: string;
   'Short bio'?: string;
@@ -13,34 +11,60 @@ interface TextInfo {
 }
 
 interface BlockInfoProps {
+  title: string;
   classPhoto: string;
   link: string;
   textInfo: TextInfo;
   simpleText?: string;
+  github?: string;
   isEvenBlock: boolean;
 }
 
-export function CommandBlock({ classPhoto, link, textInfo, simpleText, isEvenBlock }: BlockInfoProps): JSX.Element {
+export function CommandBlock({
+  title,
+  classPhoto,
+  link,
+  textInfo,
+  simpleText,
+  github,
+  isEvenBlock,
+}: BlockInfoProps): JSX.Element {
   return (
     <div className={classNames(styles.blockInfo, { [styles.rotate as string]: isEvenBlock })}>
       <Link to={link} className={styles.linkImage}>
         <div className={classNames(styles.imageCommand, styles[classPhoto])} />
       </Link>
       <div className={styles.textInfo}>
-        <h2>{textInfo.title} </h2>
-        {textInfo.github && (
+        <h2>{title} </h2>
+        {github && (
           <div>
-            Github:{' '}
+            <span className={styles.infoTitle}>Github:</span>{' '}
             <Link to={link} className={styles.link}>
-              {textInfo.github}
+              {github}
             </Link>
           </div>
         )}
 
-        {textInfo.role && <div>Role: {textInfo.role}</div>}
-        {textInfo.contributions && <div>Contributions: {textInfo.contributions}</div>}
-        {textInfo['Short bio'] && <div>Short bio: {textInfo['Short bio']}</div>}
-        {textInfo['Soft stack'] && <div>Soft stack: {textInfo['Soft stack']}</div>}
+        {textInfo.role && (
+          <div>
+            <span className={styles.infoTitle}>Role:</span> {textInfo.role}
+          </div>
+        )}
+        {textInfo.contributions && (
+          <div>
+            <span className={styles.infoTitle}>Contributions:</span> {textInfo.contributions}
+          </div>
+        )}
+        {textInfo['Short bio'] && (
+          <div>
+            <span className={styles.infoTitle}>Short bio:</span> {textInfo['Short bio']}
+          </div>
+        )}
+        {textInfo['Soft stack'] && (
+          <div>
+            <span className={styles.infoTitle}>Soft stack:</span> {textInfo['Soft stack']}
+          </div>
+        )}
         {simpleText && <div>{simpleText}</div>}
       </div>
     </div>
--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -6,54 +6,55 @@ import styles from './styles.module.scss';
 
 const commandInfo = [
   {
-    classPhoto: 'imageIzy',
     title: 'Ivan Zhirebny',
+    classPhoto: 'imageIzy',
     link: 'https://github.com/izy-code',
     textInfo: {
-      title: 'Ivan Zhirebny',
-      github: 'izy-code',
       role: 'web-developer',
       contributions: 'Routing, Detail page, Header',
       'Short bio':
         ' I graduated from the Bauman Moscow State Technical University, completed the Frontend Developer program at the HTML Academy',
       'Soft stack': 'CSS, Sass, HTML, JS, Java, TS, Webpack, Vite, Jest, React',
     },
+    github: 'izy-code',
     simpleText: '',
   },
   {
+    title: 'Vadim Kolymbet',
     classPhoto: 'imageVadim',
     link: 'https://github.com/VadimKol',
     textInfo: {
-      title: 'Vadim Kolymbet',
-      github: 'vadimkol',
       role: 'web-developer',
       contributions: 'E-commerce, Login, Catalog',
       'Short bio':
         'I graduated from Saratov State University at two thousand thirteen. I studied in the Faculty of Computer Science and Information Technologies and in the Department of Computer Security and Cryptography Theory.',
       'Soft stack': 'C/C++, PowerScript, BASH, CSS, Sass, SQL, HTML, JS, TS, Webpack, Vite, Jest, React',
     },
+    github: 'vadimkol',
     simpleText: '',
   },
   {
+    title: 'Aleksandra Bodnar',
     classPhoto: 'imageAlex',
     link: 'https://github.com/BodnarAlex',
     textInfo: {
-      title: 'Aleksandra Bodnar',
-      github: 'bodnaralex',
       role: ' web-developer, scrum-master',
-      contributions: 'Routing',
+      contributions: 'Design, Registration, Home, About',
       'Short bio':
-        'I chose the specialty Business Informatics. I think it sounds weirder in English. But        incredible experience, and in the end the best and widest choice of professions. We were well trained        in the basics of business and marketing, business processes and SRM, and most importantly programming.        This is html, css, js, writing websites and connecting them to WordPress, also Python, C++ and SQL.',
+        'I chose the specialty Business Informatics. I think it sounds weirder in English. But incredible experience, and in the end the best and widest choice of professions. We were well trained in the basics of business and marketing, business processes and SRM, and most importantly programming.',
       'Soft stack': 'CSS, Sass, HTML, JS, Java, TS, Webpack, Vite, Jest, React',
     },
+    github: 'bodnaralex',
     simpleText: '',
   },
   {
+    title: 'RSSchool',
     classPhoto: 'imageRss',
     link: 'https://rollingscopes.com/',
     textInfo: {},
     simpleText:
-      'RSSchool (Rolling Scopes School) is an online school and community that focuses on teaching web development and programming skills. It is particularly known for its comprehensive and well-structured courses that cater to beginners as well as more experienced developers. Here are some key features and aspects of RSSchool',
+      'Rolling Scopes School is an online school and community that focuses on teaching web development and programming skills. It is particularly known for its comprehensive and well-structured courses that cater to beginners as well as more experienced developers. Here are some key features and aspects of RSSchool',
+    github: '',
   },
 ];
 
@@ -66,10 +67,12 @@ export function About(): JSX.Element {
           {commandInfo.map((info, index) => (
             <CommandBlock
               key={info.classPhoto}
+              title={info.title}
               classPhoto={info.classPhoto}
               link={info.link}
               textInfo={info.textInfo}
               simpleText={info.simpleText}
+              github={info.github}
               isEvenBlock={(index + 1) % 2 === 0}
             />
           ))}
